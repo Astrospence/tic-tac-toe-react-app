f146a66c63ee42549003ffadd978ca68abd4dd2c
Astrospence <illustrator.wood@gmail.com>
2025-09-03 16:04:32 -0500

    created app

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,168 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import { useState, useEffect } from "react"
 
 export default function Home() {
+  const [ player, setPlayer ] = useState(1)
+  const [ selectedClass, setSelectedClass ] = useState({
+    0: styles.gameDot,
+    1: styles.gameDot,
+    2: styles.gameDot,
+    3: styles.gameDot,
+    4: styles.gameDot,
+    5: styles.gameDot,
+    6: styles.gameDot,
+    7: styles.gameDot,
+    8: styles.gameDot
+  })
+  const [ tally, setTally ] = useState({
+    p1: [],
+    p2: []
+  })
+  const [ winner, setWinner ] = useState(0)
+
+  const winCon = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const select = (e) => {
+    e.preventDefault()
+    const target = parseInt(e.target.id.slice(e.target.id.length - 1))
+    player === 1 ? setTally(tally => ({...tally, p1: [...tally.p1, target]})) : setTally(tally => ({...tally, p2: [...tally.p2, target]}))
+    switch (target) {
+      case 0:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 0: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 0: styles.p2Selected}))
+        break;
+      case 1:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 1: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 1: styles.p2Selected}))
+        break;
+      case 2:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 2: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 2: styles.p2Selected}))
+        break;
+      case 3:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 3: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 3: styles.p2Selected}))
+        break;
+      case 4:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 4: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 4: styles.p2Selected}))
+        break;
+      case 5:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 5: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 5: styles.p2Selected}))
+        break;
+      case 6:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 6: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 6: styles.p2Selected}))
+        break;
+      case 7:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 7: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 7: styles.p2Selected}))
+        break;
+      case 8:
+        player === 1 ? setSelectedClass(selectedClass => ({...selectedClass, 8: styles.p1Selected})) : setSelectedClass(selectedClass => ({...selectedClass, 8: styles.p2Selected}))
+        break;
+      default:
+        setSelectedClass({
+          0: styles.gameDot,
+          1: styles.gameDot,
+          2: styles.gameDot,
+          3: styles.gameDot,
+          4: styles.gameDot,
+          5: styles.gameDot,
+          6: styles.gameDot,
+          7: styles.gameDot,
+          8: styles.gameDot
+        })
+    }
+    player === 1 ? setPlayer(2) : setPlayer(1)
+  }
+
+  const newGame = () => {
+    setSelectedClass({
+          0: styles.gameDot,
+          1: styles.gameDot,
+          2: styles.gameDot,
+          3: styles.gameDot,
+          4: styles.gameDot,
+          5: styles.gameDot,
+          6: styles.gameDot,
+          7: styles.gameDot,
+          8: styles.gameDot
+        })
+  }
+
+  const checkWin = (player) => {
+    let p1win = false
+    let p2win = false
+    for (let i = 0; i < winCon.length; i++) {
+      let count = 0
+      for (let n = 0; n < tally.p1.length; n++) {
+        if ((tally.p1[n] === winCon[i][0]) || (tally.p1[n] === winCon[i][1]) || (tally.p1[n] === winCon[i][2])) {
+          count++
+        }
+      }
+      if (count > 2) {
+        p1win = true
+        break
+      }
+    }
+    for (let i = 0; i < winCon.length; i++) {
+      let count = 0
+      for (let n = 0; n < tally.p2.length; n++) {
+        if ((tally.p2[n] === winCon[i][0]) || (tally.p2[n] === winCon[i][1]) || (tally.p2[n] === winCon[i][2])) {
+          count++
+        }
+      }
+      if (count > 2) {
+        p2win = true
+        break
+      }
+    }
+    if (p1win) {
+      console.log('p1 wins!')
+    } else if (p2win) {
+      console.log('p2 wins!')
+    }
+  }
+
+  checkWin(player)
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <h1>TIC TAC TOE</h1>
+        <p>{`Player ${player}'s turn!`}</p>
+        <div className={styles.game}>
+          <div className={styles.gameGrid}>
+            <div id='dot0' className={selectedClass[0]} onClick={select}></div>
+            <div id='dot1' className={selectedClass[1]} onClick={select}></div>
+            <div id='dot2' className={selectedClass[2]} onClick={select}></div>
+            <div id='dot3' className={selectedClass[3]} onClick={select}></div>
+            <div id='dot4' className={selectedClass[4]} onClick={select}></div>
+            <div id='dot5' className={selectedClass[5]} onClick={select}></div>
+            <div id='dot6' className={selectedClass[6]} onClick={select}></div>
+            <div id='dot7' className={selectedClass[7]} onClick={select}></div>
+            <div id='dot8' className={selectedClass[8]} onClick={select}></div>
+          </div>
+        </div>
+        <div className={styles.scoreBoard}>
+          <h2>SCORE</h2>
+          <div className={styles.scoreSection}>
+            <div className={styles.score}>
+              <p>Player 1</p>
+              <p>0</p>
+            </div>
+            <div className={styles.score}>
+              <p>Player 2</p>
+              <p>0</p>
+            </div>
+          </div>
+          <button className={styles.reset} onClick={newGame}>New Game</button>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
